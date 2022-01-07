a2a84b0f5e651018cdefdfe3e0bc55aa9c60f2c4
Mridul-web-me <md.mridu.23@gmail.com>
2022-01-07 12:25:02 +0600

    nav updated

--- src/Component/Header/HeaderTop/HeaderTop.js
+++ src/Component/Header/HeaderTop/HeaderTop.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import logo from '../../../img/logo.jpg'
 
 const HeaderTop = () => {
     return (
@@ -7,15 +9,30 @@ const HeaderTop = () => {
             <Container>
                 <Row>
                     <Col xs={6} md={4}>
-                        <h1>xs=6 md=4</h1>
+                        <p><i class="fas fa-phone"></i> 0800 1 223 023 | MON-SAT, 9AM - 5PM</p>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search for Product & Brand..." aria-label="Search" />
+                            <i class="fas fa-search" type="submit"></i>
+                        </form>
                     </Col>
                     <Col xs={6} md={4}>
-                        <h1>xs=6 md=4</h1>
-
+                        <Link to="/home">
+                            <img src={logo} alt="" width="200px" />
+                        </Link>
                     </Col>
                     <Col xs={6} md={4}>
-                        <h1>xs=6 md=4</h1>
-
+                        <div>
+                            <i class="fas fa-user"></i>
+                            <p>Login</p>
+                        </div>
+                        <div>
+                            <i class="fas fa-comments"></i>
+                            <p>Quote</p>
+                        </div>
+                        <div>
+                            <i class="fas fa-shopping-bag"></i>
+                            <p>My Basket</p>
+                        </div>
                     </Col>
                 </Row>
             </Container>
--- src/Component/Header/Nav/Nav.js
+++ src/Component/Header/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Nav.css'
 const Nav = () => {
     return (
@@ -9,10 +10,10 @@ const Nav = () => {
                     <input type="radio" name="slider" id="close-btn" />
                     <ul class="nav-links">
                         <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
-                        <li>
-                            <a href="#" class="desktop-item1">Mega Menu 1</a>
+                        <li className='nav-item'>
+                            <Link to="/" class="desktop-item1">Home & Garden <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega1" />
-                            <label for="showMega1" class="mobile-item1">Mega Menu 1</label>
+                            <label for="showMega1" class="mobile-item1">Home & Garden</label>
                             <div class="mega-box1">
                                 <div class="content">
                                     <div class="row">
@@ -21,37 +22,37 @@ const Nav = () => {
                                     <div class="row">
                                         <header>Design Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Graphics</a></li>
-                                            <li><a href="#">Vectors</a></li>
-                                            <li><a href="#">Business cards</a></li>
-                                            <li><a href="#">Custom logo</a></li>
+                                            <li><Link to="/">Graphics</Link></li>
+                                            <li><Link to="/">Vectors</Link></li>
+                                            <li><Link to="/">Business cards</Link></li>
+                                            <li><Link to="/">Custom logo</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Email Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Personal Email</a></li>
-                                            <li><a href="#">Business Email</a></li>
-                                            <li><a href="#">Mobile Email</a></li>
-                                            <li><a href="#">Web Marketing</a></li>
+                                            <li><Link to="/">Personal Email</Link></li>
+                                            <li><Link to="/">Business Email</Link></li>
+                                            <li><Link to="/">Mobile Email</Link></li>
+                                            <li><Link to="/">Web Marketing</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Security services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Site Seal</a></li>
-                                            <li><a href="#">VPS Hosting</a></li>
-                                            <li><a href="#">Privacy Seal</a></li>
-                                            <li><a href="#">Website design</a></li>
+                                            <li><Link to="/">Site Seal</Link></li>
+                                            <li><Link to="/">VPS Hosting</Link></li>
+                                            <li><Link to="/">Privacy Seal</Link></li>
+                                            <li><Link to="/">Website design</Link></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <a href="#" class="desktop-item2">Mega Menu 2</a>
+                        <li className='nav-item'>
+                            <Link to="/" class="desktop-item2">Women <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega2" />
-                            <label for="showMega2" class="mobile-item2">Mega Menu 2</label>
+                            <label for="showMega2" class="mobile-item2">Women</label>
                             <div class="mega-box2">
                                 <div class="content">
                                     <div class="row">
@@ -60,37 +61,37 @@ const Nav = () => {
                                     <div class="row">
                                         <header>Design Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Graphics</a></li>
-                                            <li><a href="#">Vectors</a></li>
-                                            <li><a href="#">Business cards</a></li>
-                                            <li><a href="#">Custom logo</a></li>
+                                            <li><Link to="/">Graphics</Link></li>
+                                            <li><Link to="/">Vectors</Link></li>
+                                            <li><Link to="/">Business cards</Link></li>
+                                            <li><Link to="/">Custom logo</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Email Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Personal Email</a></li>
-                                            <li><a href="#">Business Email</a></li>
-                                            <li><a href="#">Mobile Email</a></li>
-                                            <li><a href="#">Web Marketing</a></li>
+                                            <li><Link to="/">Personal Email</Link></li>
+                                            <li><Link to="/">Business Email</Link></li>
+                                            <li><Link to="/">Mobile Email</Link></li>
+                                            <li><Link to="/">Web Marketing</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Security services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Site Seal</a></li>
-                                            <li><a href="#">VPS Hosting</a></li>
-                                            <li><a href="#">Privacy Seal</a></li>
-                                            <li><a href="#">Website design</a></li>
+                                            <li><Link to="/">Site Seal</Link></li>
+                                            <li><Link to="/">VPS Hosting</Link></li>
+                                            <li><Link to="/">Privacy Seal</Link></li>
+                                            <li><Link to="/">Website design</Link></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <a href="#" class="desktop-item3">Mega Menu 3</a>
+                        <li className='nav-item'>
+                            <Link to="/" class="desktop-item3">Men <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega3" />
-                            <label for="showMega3" class="mobile-item3">Mega Menu 3</label>
+                            <label for="showMega3" class="mobile-item3">Men</label>
                             <div class="mega-box3">
                                 <div class="content">
                                     <div class="row">
@@ -99,37 +100,37 @@ const Nav = () => {
                                     <div class="row">
                                         <header>Design Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Graphics</a></li>
-                                            <li><a href="#">Vectors</a></li>
-                                            <li><a href="#">Business cards</a></li>
-                                            <li><a href="#">Custom logo</a></li>
+                                            <li><Link to="/">Graphics</Link></li>
+                                            <li><Link to="/">Vectors</Link></li>
+                                            <li><Link to="/">Business cards</Link></li>
+                                            <li><Link to="/">Custom logo</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Email Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Personal Email</a></li>
-                                            <li><a href="#">Business Email</a></li>
-                                            <li><a href="#">Mobile Email</a></li>
-                                            <li><a href="#">Web Marketing</a></li>
+                                            <li><Link to="/">Personal Email</Link></li>
+                                            <li><Link to="/">Business Email</Link></li>
+                                            <li><Link to="/">Mobile Email</Link></li>
+                                            <li><Link to="/">Web Marketing</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Security services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Site Seal</a></li>
-                                            <li><a href="#">VPS Hosting</a></li>
-                                            <li><a href="#">Privacy Seal</a></li>
-                                            <li><a href="#">Website design</a></li>
+                                            <li><Link to="/">Site Seal</Link></li>
+                                            <li><Link to="/">VPS Hosting</Link></li>
+                                            <li><Link to="/">Privacy Seal</Link></li>
+                                            <li><Link to="/">Website design</Link></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <a href="#" class="desktop-item4">Mega Menu 4</a>
+                        <li className='nav-item'>
+                            <Link to="/" class="desktop-item4">Beauty <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega4" />
-                            <label for="showMega4" class="mobile-item4">Mega Menu 4</label>
+                            <label for="showMega4" class="mobile-item4">Beauty</label>
                             <div class="mega-box4">
                                 <div class="content">
                                     <div class="row">
@@ -138,28 +139,67 @@ const Nav = () => {
                                     <div class="row">
                                         <header>Design Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Graphics</a></li>
-                                            <li><a href="#">Vectors</a></li>
-                                            <li><a href="#">Business cards</a></li>
-                                            <li><a href="#">Custom logo</a></li>
+                                            <li><Link to="/">Graphics</Link></li>
+                                            <li><Link to="/">Vectors</Link></li>
+                                            <li><Link to="/">Business cards</Link></li>
+                                            <li><Link to="/">Custom logo</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Email Services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Personal Email</a></li>
-                                            <li><a href="#">Business Email</a></li>
-                                            <li><a href="#">Mobile Email</a></li>
-                                            <li><a href="#">Web Marketing</a></li>
+                                            <li><Link to="/">Personal Email</Link></li>
+                                            <li><Link to="/">Business Email</Link></li>
+                                            <li><Link to="/">Mobile Email</Link></li>
+                                            <li><Link to="/">Web Marketing</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
                                         <header>Security services</header>
                                         <ul class="mega-links">
-                                            <li><a href="#">Site Seal</a></li>
-                                            <li><a href="#">VPS Hosting</a></li>
-                                            <li><a href="#">Privacy Seal</a></li>
-                                            <li><a href="#">Website design</a></li>
+                                            <li><Link to="/">Site Seal</Link></li>
+                                            <li><Link to="/">VPS Hosting</Link></li>
+                                            <li><Link to="/">Privacy Seal</Link></li>
+                                            <li><Link to="/">Website design</Link></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/" class="desktop-item5">Baby & Child <i class="fas fa-angle-down"></i></Link>
+                            <input type="checkbox" id="showMega5" />
+                            <label for="showMega5" class="mobile-item5">Baby & Child</label>
+                            <div class="mega-box5">
+                                <div class="content">
+                                    <div class="row">
+                                        <img src="img.jpg" alt="" />
+                                    </div>
+                                    <div class="row">
+                                        <header>Design Services</header>
+                                        <ul class="mega-links">
+                                            <li><Link to="/">Graphics</Link></li>
+                                            <li><Link to="/">Vectors</Link></li>
+                                            <li><Link to="/">Business cards</Link></li>
+                                            <li><Link to="/">Custom logo</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div class="row">
+                                        <header>Email Services</header>
+                                        <ul class="mega-links">
+                                            <li><Link to="/">Personal Email</Link></li>
+                                            <li><Link to="/">Business Email</Link></li>
+                                            <li><Link to="/">Mobile Email</Link></li>
+                                            <li><Link to="/">Web Marketing</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div class="row">
+                                        <header>Security services</header>
+                                        <ul class="mega-links">
+                                            <li><Link to="/">Site Seal</Link></li>
+                                            <li><Link to="/">VPS Hosting</Link></li>
+                                            <li><Link to="/">Privacy Seal</Link></li>
+                                            <li><Link to="/">Website design</Link></li>
                                         </ul>
                                     </div>
                                 </div>
