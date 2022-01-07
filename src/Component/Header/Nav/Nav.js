957c8f1591a67fcdd06fa63d793466fb5c496ae1
Mridul-web-me <md.mridu.23@gmail.com>
2022-01-08 00:33:16 +0600

    menu item added

--- src/Component/Header/Nav/Nav.js
+++ src/Component/Header/Nav/Nav.js
@@ -40,7 +40,7 @@ const Nav = () => {
                         <li className='nav-item'>
                             <Link to="/" class="desktop-item1">Home & Garden <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega1" />
-                            <label for="showMega1" class="mobile-item1">Home & Garden</label>
+                            <label for="showMega1" class="mobile-item1">Home & Garden <i class="fas fa-angle-down"></i></label>
                             <div class="mega-box1">
                                 <div class="content">
                                     {/* <div class="row">
@@ -48,6 +48,7 @@ const Nav = () => {
                                     </div> */}
                                     <div class="row">
                                         <header>Home Furnishings & Accessories</header>
+                                        <hr />
                                         <ul class="mega-links">
                                             <li><Link to="/bedding">Bedding</Link></li>
                                             <li><Link to="/pillows">Pillows</Link></li>
@@ -60,6 +61,7 @@ const Nav = () => {
                                     </div>
                                     <div class="row">
                                         <header>Cooking & Dining</header>
+                                        <hr />
                                         <ul class="mega-links">
                                             <li><Link to="/potsPans">Pots & Pans</Link></li>
                                             <li><Link to="/picnicware">Picnicware</Link></li>
@@ -84,7 +86,7 @@ const Nav = () => {
                         <li className='nav-item'>
                             <Link to="/" class="desktop-item2">Women <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega2" />
-                            <label for="showMega2" class="mobile-item2">Women</label>
+                            <label for="showMega2" class="mobile-item2">Women <i class="fas fa-angle-down"></i></label>
                             <div class="mega-box2">
                                 <div class="content">
                                     {/* <div class="row">
@@ -92,6 +94,7 @@ const Nav = () => {
                                     </div> */}
                                     <div class="row">
                                         <header>Clothing</header>
+                                        <hr />
                                         <ul class="mega-links">
                                             <li><Link to="/dresses">Dresses</Link></li>
                                             <li><Link to="/jeans">Jeans</Link></li>
@@ -109,6 +112,7 @@ const Nav = () => {
                                     </div>
                                     <div class="row">
                                         <header>Lingerie, Nightwear & Hosiery</header>
+                                        <hr />
                                         <ul class="mega-links">
                                             <li><Link to="/brasSportsBras">Bras & Sports Bras</Link></li>
                                             <li><Link to="/knickers">Nightwear</Link></li>
@@ -131,7 +135,7 @@ const Nav = () => {
                         <li className='nav-item'>
                             <Link to="/" class="desktop-item3">Men <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega3" />
-                            <label for="showMega3" class="mobile-item3">Men</label>
+                            <label for="showMega3" class="mobile-item3">Men <i class="fas fa-angle-down"></i></label>
                             <div class="mega-box3">
                                 <div class="content">
                                     {/* <div class="row">
@@ -139,6 +143,7 @@ const Nav = () => {
                                     </div> */}
                                     <div class="row">
                                         <header>Clothing</header>
+                                        <hr />
                                         <ul class="mega-links">
                                             <li><Link to="/blazers">Blazers</Link></li>
                                             <li><Link to="/jeans">Jeans</Link></li>
@@ -157,10 +162,11 @@ const Nav = () => {
                                     </div>
                                     <div class="row">
                                         <header>Nightwear, Underwear & Socks</header>
+                                        <hr />
                                         <ul class="mega-links">
                                             <li><Link to="/pyjamasNightwear">Pyjamas & Nightwear</Link></li>
-                                            <li><Link to="/Socks">Socks</Link></li>
-                                            <li><Link to="/Underwear">Underwear</Link></li>
+                                            <li><Link to="/socks">Socks</Link></li>
+                                            <li><Link to="/underwear">Underwear</Link></li>
                                         </ul>
                                     </div>
                                     {/* <div class="row">
@@ -178,37 +184,68 @@ const Nav = () => {
                         <li className='nav-item'>
                             <Link to="/" class="desktop-item4">Beauty <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega4" />
-                            <label for="showMega4" class="mobile-item4">Beauty</label>
+                            <label for="showMega4" class="mobile-item4">Beauty <i class="fas fa-angle-down"></i></label>
                             <div class="mega-box4">
                                 <div class="content">
-                                    <div class="row">
+                                    {/* <div class="row">
                                         <img src="img.jpg" alt="" />
-                                    </div>
+                                    </div> */}
                                     <div class="row">
-                                        <header>Design Services</header>
+                                        <header>Makeup</header>
+                                        <hr />
                                         <ul class="mega-links">
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"> cards</Link></li>
-                                            <li><Link to="/"> logo</Link></li>
+                                            <li><Link to="/foundations">Foundations</Link></li>
+                                            <li><Link to="/concealers">Concealers</Link></li>
+                                            <li><Link to="/eyeshadows">Eyeshadows</Link></li>
+                                            <li><Link to="/mascaras">Mascaras</Link></li>
+                                            <li><Link to="/lipsticks">Lipsticks</Link></li>
+                                            <li><Link to="/nailPolish">Nail Polish</Link></li>
+                                            <li><Link to="/makeupGiftsets">Makeup Gift sets</Link></li>
+                                            <li><Link to="/MmkeUpBrushes">Make up Brushes</Link></li>
+                                            <li><Link to="/handNailsets">Hand & Nail sets</Link></li>
+                                            <li><Link to="/makeupBags">Makeup Bags</Link></li>
+                                            <li><Link to="/makeupAccessories">Makeup Accessories</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
-                                        <header>Email Services</header>
+                                        <header>Fragrance</header>
+                                        <hr />
                                         <ul class="mega-links">
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
+                                            <li><Link to="/womensPerfume">Women's Perfume</Link></li>
+                                            <li><Link to="/perfumeGiftSets">Perfume Gift Sets</Link></li>
+                                            <li><Link to="/mensAftershave">Men's Aftershave</Link></li>
+                                            <li><Link to="/mensPerfume">Men's Perfume</Link></li>
+                                            <li><Link to="/aftershaveGiftSets">Aftershave Gift Sets</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
-                                        <header>Security services</header>
+                                        <header>Skin & Body Care</header>
+                                        <hr />
                                         <ul class="mega-links">
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
+                                            <li><Link to="/cleansers">Cleansers</Link></li>
+                                            <li><Link to="/moisturisers">Moisturisers</Link></li>
+                                            <li><Link to="/bathsShower">Baths & Shower</Link></li>
+                                            <li><Link to="/bodyCare">Body Care</Link></li>
+                                            <li><Link to="/lipBalmCare">Lip Balm & Care</Link></li>
+                                            <li><Link to="/handFootCare">Hand & Foot Care</Link></li>
+                                            <li><Link to="/beautyToolsTechnology">Beauty Tools & Technology</Link></li>
+                                            <li><Link to="/skinBodyGiftSets">Skin & Body Gift Sets</Link></li>
+                                            <li><Link to="/mensGroomingShaving">Men's Grooming, Shaving</Link></li>
+                                            <li><Link to="/mensBathBody">Mens Bath & Body</Link></li>
+                                            <li><Link to="/mensWashBags">Mens Wash Bags</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div class="row">
+                                        <header>Hair Care & Styling</header>
+                                        <hr />
+                                        <ul class="mega-links">
+                                            <li><Link to="/hairCare">Hair Care</Link></li>
+                                            <li><Link to="/shampoo">Shampoo</Link></li>
+                                            <li><Link to="/conditioner">Conditioner</Link></li>
+                                            <li><Link to="/hairTreatments">Hair Treatments</Link></li>
+                                            <li><Link to="/hairStylingProducts">Hair Styling Products</Link></li>
+                                            <li><Link to="/hairBrushes">Hair Brushes</Link></li>
+                                            <li><Link to="/electricalHairBeauty">Electrical Hair & Beauty</Link></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -217,37 +254,58 @@ const Nav = () => {
                         <li className='nav-item'>
                             <Link to="/" class="desktop-item5">Baby & Child <i class="fas fa-angle-down"></i></Link>
                             <input type="checkbox" id="showMega5" />
-                            <label for="showMega5" class="mobile-item5">Baby & Child</label>
+                            <label for="showMega5" class="mobile-item5">Baby & Child <i class="fas fa-angle-down"></i></label>
                             <div class="mega-box5">
                                 <div class="content">
-                                    <div class="row">
+                                    {/* <div class="row">
                                         <img src="img.jpg" alt="" />
-                                    </div>
+                                    </div> */}
                                     <div class="row">
-                                        <header>Design Services</header>
+                                        <header>Kids Clothing & Shoes</header>
+                                        <hr />
                                         <ul class="mega-links">
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"> cards</Link></li>
-                                            <li><Link to="/"> logo</Link></li>
+                                            <li><Link to="/shirtsTops">Shirts & Tops</Link></li>
+                                            <li><Link to="/dresses">Dresses</Link></li>
+                                            <li><Link to="/pyjamasNightwear">Pyjamas & Nightwear</Link></li>
+                                            <li><Link to="/trousersLeggings">Trousers & Leggings</Link></li>
+                                            <li><Link to="/jeans">Jeans</Link></li>
+                                            <li><Link to="/partyWear">Party Wear</Link></li>
+                                            <li><Link to="/girlsBriefs">Girls Briefs</Link></li>
+                                            <li><Link to="/socks">Socks</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
-                                        <header>Email Services</header>
+                                        <header>Girls Shoes</header>
+                                        <hr />
                                         <ul class="mega-links">
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
+                                            <li><Link to="/trainers">Trainers</Link></li>
+                                            <li><Link to="/smartShoes">Smart Shoes</Link></li>
+                                            <li><Link to="/sandals">Sandals</Link></li>
+                                            <li><Link to="/flipflopsWellingtonBoots">Flipflops Wellington Boots</Link></li>
                                         </ul>
                                     </div>
                                     <div class="row">
-                                        <header>Security services</header>
+                                        <header>Boys Clothes</header>
+                                        <hr />
                                         <ul class="mega-links">
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
-                                            <li><Link to="/"></Link></li>
+                                            <li><Link to="/shortsTrousers">Shorts & Trousers</Link></li>
+                                            <li><Link to="/shirtsTops">Shirts & Tops</Link></li>
+                                            <li><Link to="/pyjamasNightwear">Pyjamas & Nightwear</Link></li>
+                                            <li><Link to="/partywear">Party wear</Link></li>
+                                            <li><Link to="/pants">Pants</Link></li>
+                                            <li><Link to="/socks">Socks</Link></li>
+                                            <li><Link to="/hats">Hats</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div class="row">
+                                        <header>Boys Shoes</header>
+                                        <hr />
+                                        <ul class="mega-links">
+                                            <li><Link to="/trainers">Trainers</Link></li>
+                                            <li><Link to="/smartShoes">Smart Shoes</Link></li>
+                                            <li><Link to="/sandals">Sandals</Link></li>
+                                            <li><Link to="/wellingtonBoots">Wellington Boots</Link></li>
+                                            <li><Link to="/flipflops">Flipflops</Link></li>
                                         </ul>
                                     </div>
                                 </div>
